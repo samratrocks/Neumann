fd5e6ea4cb92b63d85a058ad453b10bac7f54a63
samash <samrat.shiwakoti@gmail.com>
2015-03-19 12:35:56 -0400

    Add a welcome message and a basic interface.

---
+++ neumann.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/**********************************************
+Function prototypes
+*********************************************/
+int add();
+int read();
+
+
+
+using namespace std;
+int main() {
+  cout << "*****    	Welcome to Neumann \n"
+       << "***** Please enter you program below after the ?\n"
+       << "***** You can either enter an instruction\n"
+       << "***** one a time or enter a data byte.\n";
+
+  return 0;
+}
