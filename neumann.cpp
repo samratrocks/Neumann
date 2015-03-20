99d464d0268403955584d9e4cf5df3aba29d94a6
samash <samrat.shiwakoti@gmail.com>
2015-03-20 18:49:25 -0400

    Add code to seperate operation and operand from  user input.

--- neumann.cpp
+++ neumann.cpp
@@ -17,7 +17,7 @@ int main() {
 			<< ">>> 	one a time or enter a data byte.\n";
 
 	// TODO: Write a function that displays the prompt
-	// TODO:
+	// TODO: Push the operands into an array
 
 
 	prompt();
@@ -26,7 +26,9 @@ int main() {
 
 /* 
 * Shows the user a prompt where they can enter
-* instructions that tehy want to execute.
+* instructions that they want to execute.
+* This function simply calls other functions
+* based on the operation
 */
 int prompt() {
 	string currentInstruction = "";
@@ -35,20 +37,32 @@ int prompt() {
 	int currentLine = 0;
 	
 	
-	while (currentInstruction != "-9999") {
+	while (currentInstruction != "9999") {
+		// 9999 is the command to quit I
 		// Displays the prommpt like this "[0]:" 
 		cout << "[" << currentLine << "]: ";
 		
 		cin >> currentInstruction; 
 		if (currentInstruction.length() != 4) {
-			cout << "Unknown instruction!\nHALTING!\n";
+			cout << "Instructions can only be 4 letters long!\nHALTING!\n";
 			return 1;
 		}
 		
-		//operation 
+		// Split the first two letters and assign that to operation key
+		// Split the remaining two and assign that to operand 
+		operation 	= currentInstruction.substr(0,2);
+		operand 	= currentInstruction.substr(2,2);
+		cout << operation << endl;
+		cout << operand << endl;
+		
 		
 		currentLine++;
 	}
+	
+	
+	// User pressed 9999 thus is done writing instructions!
+	// Time to execute now!
+	
 	
 	return 0;
 }
